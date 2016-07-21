6f28cf6eab76d690521ba27549498dae066fd6e9
Chyld Medford <chyld.medford@gmail.com>
2016-07-21 10:37:46 -0500

    database

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose';
 import logger from './logging';
+let uri;
 
-const db = process.env.DB;
-mongoose.connect(`mongodb://localhost/${db}`);
-logger.log('info', '[MONGODB] - Database: %s', db);
+if (process.env.VCAP_SERVICES) {
+  const vcap = JSON.parse(process.env.VCAP_SERVICES);
+  uri = vcap.mlab[0].credentials.uri;
+} else {
+  const db = process.env.DB;
+  uri = `mongodb://localhost/${db}`;
+}
+
+mongoose.connect(uri);
+logger.log('info', '[MONGODB] - Database Host: %s', mongoose.connections[0].host);
+logger.log('info', '[MONGODB] - Database Name: %s', mongoose.connections[0].name);
